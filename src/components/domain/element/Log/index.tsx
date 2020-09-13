4f6e243df55d8b56f1f58fedd015795483c25ece
S.Masakatsu <48536339+S-Masakatsu@users.noreply.github.com>
2020-09-13 17:05:55 +0900

    [Add]log none

--- src/components/domain/element/Log/index.tsx
+++ src/components/domain/element/Log/index.tsx
@@ -2,7 +2,8 @@
  * Log
  * Log Domain Element Component
  */
-import React from 'react'
+import React  from 'react'
+import styled from 'styled-components'
 
 // Components
 import {Table} from '@gui/parts'
@@ -21,5 +22,16 @@ interface Props {
 }
 
 export const Log:React.FC<Props> = ({logs}) => (
+  logs.length === 0 ?
+  <StyledText>
+    操作履歴がありません
+  </StyledText>
+  :
   <Table headers={HEADERS} bodys={logs} tdNotfirstLeft={true} />
 )
+
+const StyledText = styled.p`
+  text-align: center;
+  margin-top: 30px;
+  font-size: 1rem;
+`
