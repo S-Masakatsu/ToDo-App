94c4cf065178f9a56efe2f3ae50ae744fb3fa87b
S.Masakatsu <48536339+S-Masakatsu@users.noreply.github.com>
2020-09-13 17:38:57 +0900

    [Update]page title

--- src/components/gui/parts/ScreenWrapper/index.tsx
+++ src/components/gui/parts/ScreenWrapper/index.tsx
@@ -1,6 +1,7 @@
 /**
  * ScreenWrapper GUI Parts Component
  * @hasPadding 左右にpaddingを入れるか
+ * @pageTitle ページタイトル
  */
 import React  from 'react'
 import styled from 'styled-components'
@@ -17,14 +18,16 @@ const LAYOUT_OPTION = {
 interface Props {
   children:    React.ReactNode
   hasPadding?: boolean
+  pageTitle?:  string
 }
 
-export const ScreenWrapper:React.FC<Props> = ({children, hasPadding = true}) => (
+export const ScreenWrapper:React.FC<Props> = ({children, hasPadding = true, pageTitle}) => (
   <StyledMain>
     <Layout
       hasMargin={LAYOUT_OPTION.MARGIN}
       hasPadding={hasPadding ? LAYOUT_OPTION.PADDING : {}}
     >
+      {PageTitle(pageTitle)}
       {children}
     </Layout>
   </StyledMain>
@@ -35,4 +38,26 @@ const StyledMain = styled.main`
   overflow: hidden;
   position: relative;
   top: ${NAV_BAR_HEIGHT}px;
+`
+
+const PageTitle = (title?: string) => {
+  if (!title) return
+  return (
+    <StyledHeader>
+      <StyledTitle>{title}</StyledTitle>
+    </StyledHeader>
+  )
+}
+
+const StyledHeader = styled.header`
+  padding-bottom: 8px;
+  margin-bottom: 16px;
+  border-bottom: 1px solid #e1e4e8;
+`
+
+const StyledTitle = styled.h1`
+  text-align: left;
+  font-size: 24px;
+  margin: 0;
+  padding: 0;
 `
